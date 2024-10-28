dc6694c7015ad43a8ea265b628940eca797415a6
DannieZM <dzamoranomartin@ivytech.edu>
2024-10-28 14:16:13 -0400

    commit

---
+++ MyInfo.py
@@ -0,0 +1,14 @@
+def main():
+#user enters name
+    name = input("Your Name: ")
+#user enters address
+    address = input("Your address: ")
+#user enters phone number
+    phone_number = input("Your Phone Number: ")
+#output results
+    print("Name:", name)
+    print("Address:", address)
+    print("Telephone Number:", phone_number)
+
+if __name__ == "__main__":
+    main()
